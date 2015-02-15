84403455683bb7fa981b5b28b3d89473efc9300f
Sehmon Burnam <sehmon3@gmail.com>
2015-02-15 17:15:21 -0500

    Added divs to display item details

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Item(db.Model):
     url = db.Column(db.String(240), index=True, unique=True) 
     imgurl = db.Column(db.String(240), index=True, unique=True) 
     store = db.Column(db.String(60), index=True) 
-    price = db.Column(db.Float, index=True)
+    price = db.Column(db.String(60), index=True)
     desc = db.Column(db.String(100))
 
     def __repr__(self):
--- app/views.py
+++ app/views.py
@@ -13,16 +13,21 @@ def hello():
         db.session.delete(item)
     db.session.commit()
 
-    response = requests.get('http://www.urbanoutfitters.com/urban/catalog/search.jsp?q=red+shirt#/') 
+    response = requests.get('http://www.urbanoutfitters.com/urban/catalog/search.jsp?q=blue+shirt#/') 
     soup = bs4.BeautifulSoup(response.text)
 
     for link in soup.find_all('div', attrs={'class':'category-product'}):
-        i = Item(store='This is some text') 
+        i = Item(desc = link.find("h2").get_text(),
+                imgurl = link.find("img").get("src"),
+                price = link.find("h3").get_text())
         db.session.add(i)
 
     db.session.commit() 
     
     clothes = Item.query.all()
     arrayLen = len(clothes)
+    rows = arrayLen/4
 
-    return render_template('items.html', clothes = clothes, arrayLen = arrayLen)
+    return render_template('items.html',
+            clothes = clothes,
+            arrayLen = arrayLen, rows = rows)
