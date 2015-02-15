f8a7d6af32ff2d169e5c8d2833c89e6332f6f6b4
Sehmon Burnam <sehmon3@gmail.com>
2015-02-15 15:40:17 -0500

    Post CodeDay update, got Beautiful Soup and Database to work

--- app/views.py
+++ app/views.py
@@ -1,9 +1,28 @@
 from flask import render_template, flash, redirect
 from app import app, db
 from models import Item 
+import requests
+import bs4
 
 @app.route("/")
 @app.route("/index")
 def hello():
+
+    items = Item.query.all()
+    for item in items:
+        db.session.delete(item)
+    db.session.commit()
+
+    response = requests.get('http://www.urbanoutfitters.com/urban/catalog/search.jsp?q=red+shirt#/') 
+    soup = bs4.BeautifulSoup(response.text)
+
+    for link in soup.find_all('div', attrs={'class':'category-product'}):
+        i = Item(store='This is some text') 
+        db.session.add(i)
+
+    db.session.commit() 
+    
     clothes = Item.query.all()
-    return render_template('items.html', clothes = clothes)
+    arrayLen = len(clothes)
+
+    return render_template('items.html', clothes = clothes, arrayLen = arrayLen)
